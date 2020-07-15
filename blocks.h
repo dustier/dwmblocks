41d160d4ac9167f33f66db0419fcba6f180ca501
dustier <ycliu613@outlook.com>
2020-07-15 21:55:37 +0800

    support show datetime and status of (wifi connection, battery, volume)

--- blocks.h
+++ blocks.h
@@ -3,16 +3,14 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"", "cat ~/.pacupdate | sed /📦0/d",					0,		9}, */
 
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	/* {"", "~/bin/statusbar/volume",						0,		10}, */
-
+	/* {"", "sensors | awk '/^temp1:/{print $2}'",				5,		0}, */
+	/* {"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
+    {"", "internet.bash", 5, 0},
+    {"", "battery.bash", 10, 0},
+    {"", "volume.bash", 0, 10},
+    {"", "datetime.bash", 45, 0},
 	// {"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-
 	/* {"", "~/bin/statusbar/battery",						5,		0}, */
-
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-
 	/* {"", "~/bin/statusbar/clock",						5,		0}, */
 };
 
